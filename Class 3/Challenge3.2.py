68f98bc3df82232f1654838ac688990e693a2ec2
Katarzyna Kos <60273132+kkashii@users.noreply.github.com>
2020-02-14 13:30:29 -0500

    Update Challenge3.2.py

--- Class 3/Challenge3.2.py
+++ Class 3/Challenge3.2.py
@@ -1,7 +1,11 @@
+#Creating arguements
+
 import sys
 
+# Creating an empty list for arguements
 word = []
 
+# Simple code attempting to count the occurances of the letter 'a' in a word
 def letter_count(word, char='a'):
     count = 0
     for letter in word:
@@ -9,7 +13,7 @@ def letter_count(word, char='a'):
             count += 1
     return count
 
-
+# The three arguements (when you run the code only 1 agruments works at a time?)
 word.append(sys.argv[0])
 word.append(sys.argv[1])
 word.append(sys.argv[2])
